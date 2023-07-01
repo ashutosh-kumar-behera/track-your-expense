19f7c567244891fa54eb6c4f646fb5d95c9a720d
abehera01 <abehera920@gmail.com>
2023-07-01 07:08:26 +0530

    add sorting functionality

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -11,7 +11,9 @@ export const ExpenseTable = ({
   const [filter, setQuery] = useFilter(expenses, (data) => data.category);
   const [menuPosition, setMenuPosition] = useState({});
   const [rowId, setRowId] = useState();
-  const total = filter.reduce((acc, curr) => acc + +curr.amount, 0);
+  const [callbackSort, setCallbackSort]=useState(()=>()=>{});
+  
+  const total = filter.sort(callbackSort).reduce((acc, curr) => acc + +curr.amount, 0);
 
   return (
     <>
@@ -24,12 +26,51 @@ export const ExpenseTable = ({
         expenses={expenses}
         setEditingRowId={setEditingRowId}
       />
-      <table className="expense-table" onClick={() => setMenuPosition({})}>
+      <table
+        className="expense-table"
+        onClick={() => {
+          if (menuPosition.left) {
+            setMenuPosition({});
+          }
+        }}
+      >
         <thead>
           <tr>
-            <th>Title</th>
+            <th className="amount-column">
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  onClick={() => {
+                    setCallbackSort(
+                      () => (a, b) => a.title.localeCompare(b.title)
+                    );
+                  }}
+                >
+                  <title>Ascending</title>
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                  onClick={() => {
+                    setCallbackSort(
+                      () => (a, b) => b.title.localeCompare(a.title)
+                    );
+                  }}
+                >
+                  <title>Descending</title>
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
             <th>
-              <select onChange={(e) => setQuery(e.target.value)}>
+              <select onChange={(e) => setQuery(e.target.value.toLowerCase())}>
                 <option value="">All</option>
                 <option value="grocery">Grocery</option>
                 <option value="clothes">Clothes</option>
@@ -46,6 +87,9 @@ export const ExpenseTable = ({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() => {
+                    setCallbackSort(() => (a, b) => a.amount - b.amount);
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -55,6 +99,9 @@ export const ExpenseTable = ({
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={() => {
+                    setCallbackSort(() => (a, b) => b.amount - a.amount);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -85,7 +132,14 @@ export const ExpenseTable = ({
           })}
           <tr>
             <th>Total</th>
-            <th></th>
+            <th
+              className="clear-sort"
+              onClick={() => {
+                setCallbackSort(() => () => {});
+              }}
+            >
+              Clear Sort
+            </th>
             <th>₹{total}</th>
           </tr>
         </tbody>
